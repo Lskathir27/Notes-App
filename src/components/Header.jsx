6f9119928dfbf0b0c2a89b21fca3b072e6f6ce03
Kathirvelu M <86099887+Lskathir27@users.noreply.github.com>
2021-11-20 20:36:13 +0530

    First Commit
    Version 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HighlightIcon from '@material-ui/icons/Highlight'
+
+function Header() {
+  return (
+    <header>
+      <HighlightIcon />
+      <span>Notes App</span>
+    </header>
+  )
+}
+
+export default Header
